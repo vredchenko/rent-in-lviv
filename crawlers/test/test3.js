06432eba9dab42c37dfe0c10484742c2fba8c56b
wserver <wserver@wserver.(none)>
2014-01-31 15:07:05 +0400

    Revert "added roks.js  into test folder."
    This reverts commit 58caccb25538f1cc9296060e2cb83a8e0e8c644f.

--- crawlers/test/test3.js
+++
@@ -1,7 +0,0 @@
-var request = require('request');
-var cheerio = require('cheerio');
-request('https://news.ycombinator.com', function (error, response, html) {
-  if (!error && response.statusCode == 200) {
-    console.log(html);
-  }
-});
